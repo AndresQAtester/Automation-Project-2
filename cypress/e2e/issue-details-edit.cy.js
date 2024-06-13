95fe301562ed0cbf94c2b5238d501476905d50d5
AndresQAtester <andrestammjr@gmail.com>
2024-06-13 10:38:35 +0300

    Assignment 3

--- cypress/e2e/issue-details-edit.cy.js
+++ cypress/e2e/issue-details-edit.cy.js
@@ -1,65 +1,157 @@
+const BASE_URL = Cypress.env('baseUrl');
+const ISSUE_TYPE = 'Task';
+const MODAL_SELECTOR = '[data-testid="modal:issue-details"]';
+const PRIORITY_SELECTION = '[data-testid="select:priority"]';
+const SELECT_OPTIONS = '[data-testid^="select-option:"]';
+const REPORTER_SELECTION = '[data-testid="select:reporter"]';
+const BACKLOG_LIST = '[data-testid="board-list:backlog"]';
+const UNTRIMMED_TITLE = 'Untrimmed     Title for    Bonus   Tests';
+
+let priorities = [];
+const EXPECTED_LENGTH_PRIORITY = 5;
+
 describe('Issue details editing', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
+    cy.url().should('eq', `${BASE_URL}project`).then((url) => {
       cy.visit(url + '/board');
-      cy.contains('This is an issue of type: Task.').click();
+      cy.contains(`This is an issue of type: ${ISSUE_TYPE}.`).click();
     });
   });
 
   it('Should update type, status, assignees, reporter, priority successfully', () => {
-    getIssueDetailsModal().within(() => {
-      cy.get('[data-testid="select:type"]').click('bottomRight');
-      cy.get('[data-testid="select-option:Story"]')
-          .trigger('mouseover')
-          .trigger('click');
-      cy.get('[data-testid="select:type"]').should('contain', 'Story');
-
-      cy.get('[data-testid="select:status"]').click('bottomRight');
-      cy.get('[data-testid="select-option:Done"]').click();
-      cy.get('[data-testid="select:status"]').should('have.text', 'Done');
-
-      cy.get('[data-testid="select:assignees"]').click('bottomRight');
-      cy.get('[data-testid="select-option:Lord Gaben"]').click();
-      cy.get('[data-testid="select:assignees"]').click('bottomRight');
-      cy.get('[data-testid="select-option:Baby Yoda"]').click();
-      cy.get('[data-testid="select:assignees"]').should('contain', 'Baby Yoda');
-      cy.get('[data-testid="select:assignees"]').should('contain', 'Lord Gaben');
-
-      cy.get('[data-testid="select:reporter"]').click('bottomRight');
-      cy.get('[data-testid="select-option:Pickle Rick"]').click();
-      cy.get('[data-testid="select:reporter"]').should('have.text', 'Pickle Rick');
-
-      cy.get('[data-testid="select:priority"]').click('bottomRight');
-      cy.get('[data-testid="select-option:Medium"]').click();
-      cy.get('[data-testid="select:priority"]').should('have.text', 'Medium');
-    });
+    updateIssueDetails();
   });
 
   it('Should update title, description successfully', () => {
-    const title = 'TEST_TITLE';
-    const description = 'TEST_DESCRIPTION';
-
-    getIssueDetailsModal().within(() => {
-      cy.get('textarea[placeholder="Short summary"]')
-        .clear()
-        .type(title)
-        .blur();
-
-      cy.get('.ql-snow')
-        .click()
-        .should('not.exist');
-
-      cy.get('.ql-editor').clear().type(description);
-
-      cy.contains('button', 'Save')
-        .click()
-        .should('not.exist');
-
-      cy.get('textarea[placeholder="Short summary"]').should('have.text', title);
-      cy.get('.ql-snow').should('have.text', description);
-    });
+    updateTitleAndDescription('TEST_TITLE', 'TEST_DESCRIPTION');
   });
 
-  const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
+  it('Should check the options of Priority dropdown', () => {
+    checkPriorityDropdownOptions();
+  });
+
+  it('Should check that reporter name has only characters in it', () => {
+    checkReporterNameCharacters();
+  });
+
+  it('Should remove unnecessary spaces on the board view', () => {
+    removeUnnecessarySpaces();
+  });
 });
+
+const getIssueDetailsModal = () => cy.get(MODAL_SELECTOR);
+
+function updateIssueDetails() {
+  getIssueDetailsModal().within(() => {
+    selectDropdownOption('type', 'Story');
+    verifyDropdownOption('type', 'Story');
+
+    selectDropdownOption('status', 'Done');
+    verifyDropdownOption('status', 'Done');
+
+    selectMultipleDropdownOptions('assignees', ['Lord Gaben', 'Baby Yoda']);
+    verifyDropdownOptions('assignees', ['Lord Gaben', 'Baby Yoda']);
+
+    selectDropdownOption('reporter', 'Pickle Rick');
+    verifyDropdownOption('reporter', 'Pickle Rick');
+
+    selectDropdownOption('priority', 'Medium');
+    verifyDropdownOption('priority', 'Medium');
+  });
+}
+
+function updateTitleAndDescription(title, description) {
+  getIssueDetailsModal().within(() => {
+    cy.get('textarea[placeholder="Short summary"]').clear().type(title).blur();
+    cy.get('.ql-snow').click().should('not.exist');
+    cy.get('.ql-editor').clear().type(description);
+    cy.contains('button', 'Save').click().should('not.exist');
+    cy.get('textarea[placeholder="Short summary"]').should('have.value', title);
+    cy.get('.ql-snow').should('contain.text', description);
+  });
+}
+
+function checkPriorityDropdownOptions() {
+  getIssueDetailsModal().within(() => {
+    cy.get(PRIORITY_SELECTION).invoke('text').then((selectedPriority) => {
+      priorities.push(selectedPriority);
+      cy.log(`Length of array: ${priorities.length}`);
+    });
+    cy.get(PRIORITY_SELECTION).click();
+    cy.get(SELECT_OPTIONS).each(($el) => {
+      cy.wrap($el).invoke('text').then((priorityOption) => {
+        priorities.push(priorityOption);
+        cy.log(`Value added: ${priorityOption}`);
+        cy.log(`Length of array: ${priorities.length}`);
+      });
+    }).then(() => {
+      expect(priorities.length).to.eq(EXPECTED_LENGTH_PRIORITY);
+      cy.log(`Priorities array: ${priorities}`);
+    });
+  });
+}
+
+function checkReporterNameCharacters() {
+  getIssueDetailsModal().within(() => {
+    cy.get(REPORTER_SELECTION).invoke('text').then((reporterName) => {
+      cy.log(`Reporter name: ${reporterName}`);
+      expect(reporterName).to.match(/^[A-Za-z\s]+$/);
+    });
+    cy.get(REPORTER_SELECTION).click();
+    cy.get(SELECT_OPTIONS).should('be.visible');
+    cy.get(SELECT_OPTIONS).each(($el) => {
+      cy.wrap($el).invoke('text').then((reporterName) => {
+        cy.log(`Reporter name: ${reporterName}`);
+        expect(reporterName).to.match(/^[A-Za-z\s]+$/);
+      });
+    });
+  });
+}
+
+function removeUnnecessarySpaces() {
+  closeIssueDetailView();
+  openCreateNewIssue();
+  getIssueTitle().click().type(UNTRIMMED_TITLE);
+  clickCreateIssueButton();
+  ensureIssueIsCreated();
+
+  cy.get(BACKLOG_LIST).children().first().find('p').invoke('text').should('eq', UNTRIMMED_TITLE.trim());
+}
+
+const selectDropdownOption = (field, option) => {
+  cy.get(`[data-testid="select:${field}"]`).click('bottomRight');
+  cy.get(`[data-testid="select-option:${option}"]`).click();
+};
+
+const verifyDropdownOption = (field, option) => {
+  cy.get(`[data-testid="select:${field}"]`).should('contain.text', option);
+};
+
+const selectMultipleDropdownOptions = (field, options) => {
+  options.forEach(option => {
+    cy.get(`[data-testid="select:${field}"]`).click('bottomRight');
+    cy.get(`[data-testid="select-option:${option}"]`).click();
+  });
+};
+
+const verifyDropdownOptions = (field, options) => {
+  options.forEach(option => {
+    cy.get(`[data-testid="select:${field}"]`).should('contain.text', option);
+  });
+};
+
+const closeIssueDetailView = () => cy.get('[data-testid="icon:close"]').first().click();
+const openCreateNewIssue = () => cy.get('[data-testid="icon:plus"]').click();
+const getIssueTitle = () => cy.get('[data-testid="form-field:title"]');
+const clickCreateIssueButton = () => cy.get('button[type="submit"]').click();
+
+function ensureIssueIsCreated() {
+  cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+  cy.contains('Issue has been successfully created.').should('be.visible');
+  cy.reload();
+  cy.contains('Issue has been successfully created.').should('not.exist');
+  cy.get(BACKLOG_LIST).should('be.visible').and('have.length', 1).within(() => {
+    cy.get('[data-testid="list-issue"]').should('have.length', 5);
+  });
+}
